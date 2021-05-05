89f07da64b339e71462562692a288abeba8cc6a6
Vinicios-Durigan <62732923+Vinicios-Durigan@users.noreply.github.com>
2021-05-05 16:56:31 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace G7_update
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.lb_att = new Guna.UI.WinForms.GunaLabel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.guna2WinProgressIndicator1 = new Guna.UI2.WinForms.Guna2WinProgressIndicator();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(51)))), ((int)(((byte)(57)))));
+            this.panel1.Controls.Add(this.guna2CirclePictureBox1);
+            this.panel1.Controls.Add(this.gunaLabel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 46);
+            this.panel1.TabIndex = 3;
+            // 
+            // guna2CirclePictureBox1
+            // 
+            this.guna2CirclePictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2CirclePictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2CirclePictureBox1.Image")));
+            this.guna2CirclePictureBox1.Location = new System.Drawing.Point(10, 12);
+            this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox1.ShadowDecoration.Parent = this.guna2CirclePictureBox1;
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(30, 24);
+            this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2CirclePictureBox1.TabIndex = 3;
+            this.guna2CirclePictureBox1.TabStop = false;
+            this.guna2CirclePictureBox1.UseTransparentBackground = true;
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.gunaLabel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gunaLabel1.Location = new System.Drawing.Point(46, 12);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(165, 21);
+            this.gunaLabel1.TabIndex = 0;
+            this.gunaLabel1.Text = "Sistema de Update 2.0";
+            // 
+            // lb_att
+            // 
+            this.lb_att.AutoSize = true;
+            this.lb_att.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lb_att.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb_att.Location = new System.Drawing.Point(76, 58);
+            this.lb_att.Name = "lb_att";
+            this.lb_att.Size = new System.Drawing.Size(174, 21);
+            this.lb_att.TabIndex = 4;
+            this.lb_att.Text = "Procurando Atualização";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            // 
+            // guna2WinProgressIndicator1
+            // 
+            this.guna2WinProgressIndicator1.AnimationSpeed = 50;
+            this.guna2WinProgressIndicator1.AutoStart = true;
+            this.guna2WinProgressIndicator1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2WinProgressIndicator1.CircleSize = 1F;
+            this.guna2WinProgressIndicator1.Location = new System.Drawing.Point(118, 93);
+            this.guna2WinProgressIndicator1.Name = "guna2WinProgressIndicator1";
+            this.guna2WinProgressIndicator1.ProgressColor = System.Drawing.Color.Gray;
+            this.guna2WinProgressIndicator1.Size = new System.Drawing.Size(62, 62);
+            this.guna2WinProgressIndicator1.TabIndex = 6;
+            this.guna2WinProgressIndicator1.UseTransparentBackground = true;
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.gunaLabel2.Location = new System.Drawing.Point(12, 160);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(51, 15);
+            this.gunaLabel2.TabIndex = 5;
+            this.gunaLabel2.Text = "Version: ";
+            // 
+            // gunaElipse1
+            // 
+            this.gunaElipse1.Radius = 6;
+            this.gunaElipse1.TargetControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(57)))), ((int)(((byte)(63)))));
+            this.ClientSize = new System.Drawing.Size(310, 186);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lb_att);
+            this.Controls.Add(this.guna2WinProgressIndicator1);
+            this.Controls.Add(this.gunaLabel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI.WinForms.GunaLabel lb_att;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private Guna.UI2.WinForms.Guna2WinProgressIndicator guna2WinProgressIndicator1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
+        private Guna.UI.WinForms.GunaElipse gunaElipse1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using ChaseLabs.CLUpdate;
+using AutoIt;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace G7_update
+{
+    public partial class Form1 : Form
+    {
+        Dispatcher dis = Dispatcher.CurrentDispatcher;
+        string url = "https://www.dropbox.com/s/7zy1u9b3ox5xeun/Debug.zip?dl=1";
+        string remote_version_url = "https://www.dropbox.com/s/2kodp2ykjynizdx/version?dl=1";
+        string version_key = "application: ";
+        string global_path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"Gemini7");
+        string update_path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Gemini7", "G7", "update");
+        string app_path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Gemini7", "G7", "bin");
+        string local_version_path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Gemini7", "G7", "version");
+        string loucher_exe = "PedidosG7.exe";
+        string version;
+        string dataBase = @"\g7New.sql";
+        string uploadFile = @"\Update.sql";
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void CreateDataBase() {
+            try
+            {
+                MySqlConnection connect = new MySqlConnection("server=localhost;user=root;port=3306;password=9637443Lol;");
+                MySqlCommand command = new MySqlCommand("CREATE DATABASE IF NOT EXISTS g7;", connect);
+                connect.Open();
+                command.ExecuteNonQuery();
+                connect.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Tentativa de Criar a Database => " + ex);
+            }
+          
+        }
+        private void ImportUploadFromDb() {
+            try
+            {
+                string pth = app_path + uploadFile;
+                MySqlConnection connect = new MySqlConnection(ConfigurationManager.ConnectionStrings["client"].ConnectionString);
+                MySqlCommand cmd = new MySqlCommand("", connect);
+                string text = File.ReadAllText(pth);
+
+                cmd.CommandText = text;
+
+                connect.Open();
+                
+                cmd.ExecuteNonQuery();
+                connect.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error --> " + ex);
+            }
+          
+        }
+        private void ImportFirstDatabase() {
+            try
+            {
+                string constring = "server=localhost;user=root;pwd=9637443Lol;";
+                string file = app_path + dataBase;
+                using (MySqlConnection conn = new MySqlConnection(constring))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        using (MySqlBackup mb = new MySqlBackup(cmd))
+                        {
+                            cmd.Connection = conn;
+                            conn.Open();
+                            mb.ImportFromFile(file);
+                            conn.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error --> " + ex);
+            }
+        }
+        public bool chekLocalBase()
+        {
+          
+            MySqlConnection connect = new MySqlConnection(ConfigurationManager.ConnectionStrings["client"].ConnectionString);
+            try
+            {
+                connect.Open();
+                connect.Close();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                connect.Close();
+                return false;
+            }
+        }
+        public void ConfigDataBase()
+        {
+            var exist = chekLocalBase();
+            if (exist)
+            {
+                try
+                {
+                    importNewDb();
+                    Process.Start(app_path + @"\" + loucher_exe);
+                    Application.Exit();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Erro Entre em contato com o Administrador");
+                }
+               
+            }
+            else
+            {
+             
+                scriptInstallMAriadb();
+            }
+        }
+        public void importNewDb() {
+
+            ImportUploadFromDb();
+            MessageBox.Show("Importação Completa");
+        }
+        public void scriptInstallMAriadb()
+        {
+            Task.Run(() =>
+            {
+                lb_att.Text = "Instalando Base De Dados";
+            });
+           // CreateDataBase();
+            ImportFirstDatabase();
+
+        }
+        public void Update()
+        {
+            Task.Run(() =>
+            {
+                dis.Invoke(() =>
+                {
+                    lb_att.Text = "Procurando Atualizações . . .";
+                }, DispatcherPriority.Normal);
+                var update = Updater.Init(url, update_path, app_path, loucher_exe);
+                if(UpdateManager.CheckForUpdate(version_key, local_version_path, remote_version_url))
+                {
+                    dis.Invoke(() =>
+                    {
+
+                        lb_att.Text = "Atualização Encontrada ! !";
+
+                    }, DispatcherPriority.Normal);
+                    dis.Invoke(() =>
+                    {
+
+                        lb_att.Text = "Fazendo Download . . .";
+
+                    }, DispatcherPriority.Normal);
+                    update.Download();
+                    dis.Invoke(() =>
+                    {
+                        lb_att.Text = "Configurando Arquivos . . .";
+                    }, DispatcherPriority.Normal);
+                    update.Unzip();
+                    dis.Invoke(() =>
+                    {
+                        lb_att.Text = "Atualização Finalizada ! !";
+                    }, DispatcherPriority.Normal);
+                    update.CleanUp();
+                    using (var client = new System.Net.WebClient())
+                    {
+                        client.DownloadFile(remote_version_url, local_version_path);
+                    }
+                    dis.Invoke(() =>
+                    {
+                        lb_att.Text = "Configurando Databases";
+                    }, DispatcherPriority.Normal);
+
+                    ConfigDataBase();
+                    Application.Exit();
+
+                }
+                else
+                    dis.Invoke(() =>
+                    {
+                        Process.Start(app_path + @"\" + loucher_exe);
+                        Application.Exit();
+                    }, DispatcherPriority.Normal);
+            });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (UpdateManager.CheckForUpdate(version_key, local_version_path, remote_version_url))
+            {
+               if(Directory.Exists(global_path)){
+                    System.IO.Directory.Delete(global_path, true);
+                }
+                Update();
+            }
+            else
+            {
+                Update();
+            }
+        }
+    }
+}
